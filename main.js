8032793d54296219fbd08233a9392e95cc24415b
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-09-09 01:04:58 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+window.addEventListener("mousemove", function(details){
+    let xval = gsap.utils.mapRange(0, window.innerWidth, 200 + rect.getBoundingClientRect().width/2, window.innerWidth - (200 + rect.getBoundingClientRect().width/2) , details.clientX)
+    gsap.to("#rect",{
+        left: xval + "px",
+        ease: Power3
+    })
+})
